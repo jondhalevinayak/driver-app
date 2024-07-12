eb650ccfcc76b261d66b809742d3078aa4f7d9ea
jondhalevinayak <jondhalevinayak2@gmail.com>
2024-07-11 19:01:09 -0500

    Initial commit

---
+++ src/main/java/com/driver_app/DriverAppApplication.java
@@ -0,0 +1,15 @@
+package com.driver_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DriverAppApplication.class, args);
+
+	}
+
+}
